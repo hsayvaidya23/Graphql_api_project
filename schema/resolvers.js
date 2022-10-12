1a81a31f0dd34b292a003e4dd04081f4d029b549
Yash Vaidya <bobbynaldocr7@gmail.com>
2022-10-12 23:52:27 +0530

    mutations update and delete

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -36,6 +36,24 @@ const resolvers = {
             UserList.push(user);
             return user;
         },
+
+        updateUsername: (parent, args) => {
+            const { id, newUsername } = args.input;
+            let userUpdated;
+            UserList.forEach((user) => {
+                if(user.id === Number(id)){
+                    user.username = newUsername;
+                    userUpdated = user;
+                }
+            });
+            return userUpdated;
+        },
+        
+        deleteUser: (parent, args) => {
+            const id = args.id;
+            _.remove(UserList, (user) => user.id === Number(id));
+            return null;
+        }
     },
 };
 
