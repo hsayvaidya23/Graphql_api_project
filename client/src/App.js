a8b30da69cdca20e285dd66c6f1cf9e47eebf453
Yash Vaidya <bobbynaldocr7@gmail.com>
2022-11-05 22:39:14 +0530

    useQuery-hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import DisplayData from "./DisplayData";
 
 function App() {
   const client = new ApolloClient({
     cache: new InMemoryCache(),
-    uri: "http://localhost:3001/graphql",
+    uri: "http://localhost:4000/graphql",
   });
   return (
     <ApolloProvider client={client}>
-      <div className="App">Hello World!</div>
+      <div className="App"><DisplayData /></div>
     </ApolloProvider>
   );
 }
---
+++ client/src/DisplayData.js
@@ -0,0 +1,107 @@
+import { useQuery, gql, useLazyQuery } from "@apollo/client";
+import React, { useState } from "react";
+
+const QUERY_ALL_USERS = gql`
+  query GetAllUsers {
+    users {
+      id
+      name
+      age
+      username
+      nationality
+    }
+  }
+`;
+const QUERY_ALL_MOVIES = gql`
+  query GetAllMovies {
+    movies {
+      name
+    }
+  }
+`;
+
+const GET_MOVIE_BY_NAME = gql `
+  query Movie($name: String!) {
+    movie(name: $name) {
+      name 
+      yearOfPublication
+    }
+  }
+`;
+
+const DisplayData = () => {
+  const [movieSearched, setMovieSearched] = useState("");
+
+  const { data, loading, error } = useQuery(QUERY_ALL_USERS);
+  const { data: movieData } = useQuery(QUERY_ALL_MOVIES);
+
+  const [
+    fetchMovie,
+    { data: movieSearchedData, error: movieError },
+  ] = useLazyQuery(GET_MOVIE_BY_NAME);
+
+  if (loading) {
+    return <h1>DATA IS LOADING...</h1>;
+  }
+
+  if (error) {
+    console.log(error);
+  }
+
+  if (data) {
+    console.log(data);
+  }
+
+  if (movieError) {
+    console.log(movieError);
+  }
+
+  return (
+    <div>
+      {data &&
+        data.users.map((user) => {
+          return (
+            <div>
+              <h1>Name: {user.name}</h1>
+              <h1>Username: {user.username}</h1>
+              <h1>Age: {user.age}</h1>
+              <h1>Nationality: {user.nationality}</h1>
+            </div>
+          );
+        })}
+      {movieData &&
+        movieData.movies.map((movie) => {
+          return <h1>Movie Name: {movie.name} </h1>;
+        })}
+
+      <div>
+        <input
+          type="text"
+          placeholder="Interstellar"
+          onChange={(event) => {
+            setMovieSearched(event.target.value);
+          }}
+        />
+        <button onClick={() => {
+          fetchMovie({
+            variables: {
+              name: movieSearched,
+            },
+          });
+        }}
+        >Fetch Data</button>
+        <div>
+          { movieSearchedData && (
+            <div>
+              <h1>MovieName: {movieSearchedData.movie.name} </h1>
+              <h1>Year of Publication: {movieSearchedData.movie.yearOfPublication} </h1>
+            </div>
+          )}
+          { movieError && <h1>There was an error fetching the data</h1>}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DisplayData;
